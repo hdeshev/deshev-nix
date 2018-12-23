5f07c2c3f331c123d87d9a5650ea8caa77df978e
Hristo Deshev <hristo@deshev.com>
2018-12-23 10:39:16 +0200

    Fix themes by exposing the GTK 'murrine' engine in gui-wrapper

--- gui-wrapper.nix
+++ gui-wrapper.nix
@@ -1,8 +1,9 @@
-{ glibcLocales, writeShellScriptBin }:
+{ glibcLocales, writeShellScriptBin, gtk-engine-murrine }:
 name: code:
   writeShellScriptBin name ''
     export LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive
     export XCURSOR_PATH=/usr/share/icons
+    export GTK_PATH="$GTK_PATH:${gtk-engine-murrine}/lib/gtk-2.0"
 
     ${code}
     ''
