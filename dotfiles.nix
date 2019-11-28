055e4aa02e837b12b8de7e7a95e60daf610b1056
Hristo Deshev <hristo@deshev.com>
2019-11-28 13:31:11 +0200

    Fix '"' empty dir issue in dotfiles script
    Run through shellcheck and clean up warnings

--- dotfiles.nix
+++ dotfiles.nix
@@ -7,15 +7,16 @@ in
   writeShellScriptBin "dotfiles" ''
     create_symlink() {
       file="$1"
-      target="$HOME/$2"
-      dir="$(dirname \"$target\")"
+      symlink_file="$HOME/$2"
+      symlink_dir="$(dirname "$symlink_file")"
 
-      mkdir -p "$dir"
-      ln -sf "$file" "$target"
+      echo "symlink_file: $symlink_file, symlink_dir: $symlink_dir"
+      mkdir -p "$symlink_dir"
+      ln -sf "$file" "$symlink_file"
     }
 
     echo "symlinking dotfiles..."
-    sed -n '/# COMMANDS/,$p' $(which dotfiles)
+    sed -n '/# COMMANDS/,$p' "$(command -v dotfiles)"
     # COMMANDS
     ${commands}
   ''
