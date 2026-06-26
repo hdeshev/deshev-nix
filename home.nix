cc89f835bfa31aa8ebcea459ff791deea82e326c
Hristo Deshev <hristo@deshev.com>
2026-06-26 16:45:44 +0300

    docker lsp

--- home.nix
+++ home.nix
@@ -130,6 +130,8 @@ targets.genericLinux.enable = true;
     pkgs-unstable.shfmt
     pkgs-unstable.bash-language-server
 
+    # docker
+    pkgs-unstable.dockerfile-language-server
     # markdown
     pkgs-unstable.marksman
     pkgs-unstable.markdownlint-cli
