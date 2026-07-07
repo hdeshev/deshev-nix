820e07d386f676fd9e14869620def4cbcfaffaae
Hristo Deshev <hristo@deshev.com>
2026-07-08 00:04:33 +0300

    sync m2m

--- emacs.nix
+++ emacs.nix
@@ -29,9 +29,9 @@ in
       Requires = [ "emacs-wsl-env.service" ];
     };
     Service = {
-      Type = "forking";
+      Type = "exec";
       EnvironmentFile = "-%h/.cache/emacs/wsl-env";
-      ExecStart = "${emacs}/bin/emacs --daemon";
+      ExecStart = "${pkgs.fish}/bin/fish -i -c 'exec ${emacs}/bin/emacs --fg-daemon'";
       ExecStop = "${emacs}/bin/emacsclient --eval '(kill-emacs)'";
       Restart = "on-failure";
       Environment = "SSH_AUTH_SOCK=%t/keyring/ssh";
@@ -39,6 +39,28 @@ in
     Install = {
       WantedBy = [ "graphical-session.target" ];
     };
+  };
+
+  xdg.configFile."fish/conf.d/wsl.fish" = {
+    text = ''
+      # On WSL, capture Windows PATH entries (not inherited by systemd user services)
+      # and prepend them so Windows executables (powershell.exe, clip.exe, etc.) are available.
+      set cmd_exe "/mnt/c/Windows/System32/cmd.exe"
+      if test -x "$cmd_exe"
+          set windows_path ($cmd_exe /c "echo %PATH%" 2>/dev/null | tail -1 | tr -d '\r\n')
+          if test -n "$windows_path"
+              set linux_paths ""
+              for entry in (string split ';' -- "$windows_path")
+                  set entry (string trim -- "$entry")
+                  test -z "$entry"; and continue
+                  set drive (string lower -- (string sub -l 1 -- "$entry"))
+                  set rest (string replace -a '\\' '/' (string sub -s 3 -- "$entry"))
+                  set linux_paths "$linux_paths/mnt/$drive$rest:"
+              end
+              set -x PATH "$linux_paths$PATH"
+          end
+      end
+    '';
   };
 
   home.packages = [ emacs ];
